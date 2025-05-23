55acaa01fab10c4bd62021088798614de602e17e
Manishkr117 <maniskr117@gmail.com>
2025-05-23 19:51:50 +0530

    Added Delete capability

--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 class Node
 {
   public string data { get; set; }
@@ -5,7 +7,14 @@ class Node
 }
 
 class LinkedListOperations
+
 {
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="head"></param>
+  /// <param name="data"></param>
+  /// <returns></returns>
     internal Node AddElementToLinkedList(Node head, string data)
     {
       if (head.data == null && head.next == null)
@@ -27,6 +36,12 @@ class LinkedListOperations
 
     }
 
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="head"></param>
+/// <returns></returns>
     internal bool PrintAllEmenents(Node head)
     {
       if (head.next == null)
@@ -42,6 +57,12 @@ class LinkedListOperations
       return false;
     }
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="head"></param>
+/// <param name="dataToBeDeleted"></param>
+/// <returns></returns>
     internal bool DeleteGivenElementFromArray(Node head, string dataToBeDeleted)
   {
 
@@ -74,4 +95,68 @@ class LinkedListOperations
     //if traveresd and the element is not found 
     return false;
   }
+
+
+/// <summary>
+/// This method adds elemt to the linked list at given index
+/// </summary>
+/// <param name="head"></param>
+/// <param name="dataToBeAdded"></param>
+/// <param name="index"></param>
+/// <returns></returns>
+    internal bool AddElementAtGivenIndex(Node head,string dataToBeAdded, int index)
+    {
+        //traverse to reach the node whose index has been passed
+        int currentPostion=0;
+        while(currentPostion!=index-1)
+        {
+          if(head.next==null)   //If we reach end of the node.
+          return false;
+
+          Node previousNode= head;
+          head=head.next;
+          currentPostion++;
+           if(currentPostion==index-1)
+           {
+            Node n=new Node();
+            n.next=previousNode.next;
+            n.data=dataToBeAdded;
+            previousNode.next=n;
+            return true;
+           }
+        }
+       
+
+      return false;
+    }
+
+/// <summary>
+/// This method deletes an elemet form given linked list at specified position.
+/// </summary>
+/// <param name="head"></param>
+/// <param name="index"></param>
+/// <returns></returns>
+    internal bool RemoveElementAtGivenIndex(Node head,int  index)
+    {
+       //traverse to reach the node whose index has been passed
+        int currentPostion=0;
+        while(currentPostion!=index-1)
+        {
+          if(head.next==null)   //If we reach end of the node.
+          return false;
+
+          Node previousNode= head;
+          head=head.next;
+          currentPostion++;
+           if(currentPostion==index-1)
+           {
+            Node n=new Node();
+            previousNode.next= head.next;
+            head=null;
+            return true;
+           }
+        }
+        return false;
+    }
+
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -21,7 +21,7 @@ int choice=5;
 while (choice != 0)
 {
 
-    Console.WriteLine(@"PLeas provide ur inputs \r\n Press 1 to Add elements to linked list \r\n  Press 2 to Display all the elements of the linked list \r\n   Press 3 to delelet the provided element of th linked list \r\n
+    Console.WriteLine(@"PLeas provide ur inputs \r\n Press 1 to Add elements to linked list \r\n  Press 2 to Display all the elements of the linked list \r\n   Press 3 to delete the provided element of th linked list \r\n   Press 4 to insert elemet at given position\r\n    Press 5 to delete element at given index  \r\n
 Press 0 key to exit ");
 
 var input = Console.ReadLine();
@@ -44,12 +44,44 @@ int.TryParse(input, out  choice);
             }
         case 3:
             {
+                // Press 3 to delete the provided element of th linked list 
                 Console.WriteLine("PLease enter the elelemt to be deleted followed by return key");
                 string? dataToBeDeleleted = Console.ReadLine();
                 if (linkedListOperations.DeleteGivenElementFromArray(node, dataToBeDeleleted))
                     Console.WriteLine(dataToBeDeleleted + "  deleted successfully");
                 else
                     Console.WriteLine(dataToBeDeleleted + " could not be deleted");
+                break;
+            }
+            case 4:
+            {
+                //Press 4 to insert elemet at given position
+                Console.WriteLine("Press enter the index followed by enter");
+                string? index = Console.ReadLine();
+                if(int.TryParse(index,out int positionAtWhichItem2BAdded))
+                {
+                    Console.WriteLine("Press enter the element to be  followed by enter");
+                string? element = Console.ReadLine();
+                linkedListOperations.AddElementAtGivenIndex( node, element,  positionAtWhichItem2BAdded);
+                }
+                else{
+                    Console.WriteLine("Ypu have entered wrong index ");
+                }
+               
+                break;
+            }
+            case 5:
+            {
+                //\r\n    Press 5 to delete element at given index  
+                Console.WriteLine("Press enter the index followed by enter");
+                string? index = Console.ReadLine();
+                if(int.TryParse(index,out int positionAtWhichItem2BAdded))
+                {
+                linkedListOperations.RemoveElementAtGivenIndex( node,  positionAtWhichItem2BAdded);
+                }
+                else{
+                    Console.WriteLine("Ypu have entered wrong index ");
+                }
                 break;
             }
         case 0:
